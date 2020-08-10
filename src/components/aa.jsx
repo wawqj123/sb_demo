b0db211d166e3a7daddd4529f79ea9f13294f268
xiaoning <1686532381@qq.com>
2020-08-10 09:23:19 +0800

    init app

---
+++ src/components/aa.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class aa extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default aa;
